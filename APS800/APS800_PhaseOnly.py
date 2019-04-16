7e9ff7d2bf91d184450ee8657075f26e43dd78ed
Ouillz <marie.ouille@gmail.com>
2019-04-16 11:17:36 +0200

    Update APS800_PhaseOnly.py

--- APS800/APS800_PhaseOnly.py
+++ APS800/APS800_PhaseOnly.py
@@ -9,6 +9,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.gridspec as gridspec
 import os
+from itertools import takewhile
 
 # FIRST : EXTRACT DATA 
 #print ('Where is the file located? e.g : Z:\Laser\CEP\20190311')
@@ -45,9 +46,6 @@ for i in np.arange(0,nb):
     phase[i]=float(phases[i][2:7])
     squared_phase[i]=phase[i]*phase[i]
     amp[i]=float(amps[i][2:7])
-
-    
-        
 
     
 #Phase deviation rms in mrad
@@ -94,3 +92,61 @@ plt.text(0.1*max(d), max(phase)-0.1*max(phase) ,'$\sigma_{rms}$ = ' + str(ecart_
 #Save figure
 fig.suptitle('file : ' + str(files), size = s -4)
 plt.savefig( str(output_dir) + '\\' + filepath[-8::] + '_' + str(filename) + '_phase_histo.png', dpi=300,  bbox_inches='tight')
+
+
+
+
+
+
+
+
+
+
+
+def st(l, b): return len([x for x in takewhile(lambda x: x[1] <= b, enumerate(l))])
+    
+def partial (start, end):
+    ##optional : plot just one part of the graph 
+#    start = 35 #s
+#    end = 42 #s
+    a = st(time,start)
+    b = st(time, end)
+    
+    
+    #Phase deviation rms in mrad
+    mean_phase2 = sum(phase[a:b])/np.size(phase[a:b])
+    for i in np.arange(0,np.size(phase)):  #il faut le refaire parceque les phases ont été centrée sur 0 !!
+        squared_phase[i]=phase[i]*phase[i]         
+    mean_squared_phase2 = sum(squared_phase[a:b])/np.size(squared_phase[a:b])
+    ecart_type2 = int(round(np.sqrt(mean_squared_phase2 - mean_phase2*mean_phase2),3)*1000)
+    
+    
+           
+    #Figure parameters
+    fig=plt.figure(figsize=(10,3))
+    gs = gridspec.GridSpec(1, 2, wspace = 0,  width_ratios=[2, 1])
+    s=12
+    
+    #Histogram
+    plt.subplot(gs[0,1])
+    plt.hist(phase[a:b]-np.mean(phase[a:b]), bins=round(ecart_type2/20), orientation='horizontal', color='navy')  #bins =.... number of columns
+    plt.yticks([])
+    plt.xlabel('occurrence', size=s)
+    plt.ylim([-np.pi,np.pi])
+    plt.tick_params(axis='both',labelsize=s)
+    d,f=np.histogram(phase[a:b]-np.mean(phase[a:b]), bins=round(ecart_type2/20))
+    plt.axis('off')
+    plt.text(0.1*max(d), 0.9*np.pi, '$\sigma_{rms}$ = ' + str(ecart_type2) + ' mrad', size=s+1)
+    
+    #CEP as a function of time
+    plt.subplot(gs[0,0])
+    plt.plot(time[a:b],phase[a:b]-np.mean(phase[a:b]), '.', markersize=1.2,  color='navy')
+    plt.xlabel('time (sec)', size=s)
+    plt.ylabel('relative CEP (rad)', size=s)
+    plt.tick_params(axis='both',labelsize=s)
+    plt.ylim([-np.pi,np.pi])
+    plt.yticks([-np.pi, -np.pi/2,0,np.pi/2, np.pi], ['-$\pi$','-$\pi$/2', '0', '$\pi$/2','$\pi$' ], size=s)
+    
+    #Save figure
+    fig.suptitle('file : ' + str(files), size = s -4)
+    plt.savefig( str(output_dir) + '\\' + filepath[-8::] + '_' + str(filename) + '_phase_histo_interval-' + str(start) + '_' + str(end) +'.png', dpi=300,  bbox_inches='tight')
