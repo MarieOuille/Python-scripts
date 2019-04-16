906a3b9b97452b8c5702e4547ef3fd8e2bf5671f
Ouillz <marie.ouille@gmail.com>
2019-04-16 12:21:03 +0200

    Update APS800_PhaseAmplitudeVoltage.py

--- APS800/APS800_PhaseAmplitudeVoltage.py
+++ APS800/APS800_PhaseAmplitudeVoltage.py
@@ -16,18 +16,23 @@ import os
 #print ('What is the name of the .dat file? e.g : 20190301_CEPdata ')
 #filename = input('The name of the file is :')
 
-filepath = r'Z:\Laser\CEP\20180619'
-filename ='HCF-4fs-1500mbar-ter-single'
+filepath = r'Z:\Laser\CEP\20180615'
+filename ='CEP stab_30ms'
+
+
+#paramaters to be chosen :
+Pi_window = 1 # 1 to plot the CEP vs Time between -pi and +pi ; 0 if not
+lines2pi = 1  #1 to plot horizontal grey lines every 2Pi, 0 if not
+
+
+
 
 file= str(filename) + '.dat'
-
 path = os.getcwd()
 if not os.path.exists('analyzed_data'):
     os.mkdir('analyzed_data')
 output_dir=path+'\\analyzed_data'    #output directory = new directory created where this python file is located
-
 files = str(filepath) + "\\" + str(file)
-
 
 #information contained in the file header :
 with open(files) as fp:
@@ -81,8 +86,7 @@ for k in np.arange(0,np.size(indices)-1) :
         b = int(indices[k])
     elif k>0:
         b = int(indices[k])+8
-    e = int(indices[k+1]-1) 
-#    print(b,e)    
+    e = int(indices[k+1]-1)   
     voltage =[] ; time = [] ; phase = [] ; squared_phase = [] ; amp = [] #creation of the lists 
     
     for j in np.arange(b,e):
@@ -93,15 +97,14 @@ for k in np.arange(0,np.size(indices)-1) :
         voltage.append(float(voltages[j]))
         
         
-
         
     #Phase deviation rms in mrad
     mean_phase = sum(phase)/np.size(phase)
     mean_squared_phase = sum(squared_phase)/np.size(squared_phase)
     ecart_type = int(round(np.sqrt(mean_squared_phase - mean_phase*mean_phase),3)*1000)
+    
 
-
-    #center values on 0 (optional)
+    #center values on 0 
     for i in np.arange(0,np.size(phase)):
         phase[i]=phase[i]-mean_phase
 
@@ -111,6 +114,7 @@ for k in np.arange(0,np.size(indices)-1) :
     gs = gridspec.GridSpec(3, 2, wspace = 0,  width_ratios=[2, 1])
     s=12
 
+
     
     #CEP as a function of time
     CEPvsT = plt.subplot(gs[1,0])
@@ -118,14 +122,19 @@ for k in np.arange(0,np.size(indices)-1) :
     plt.ylabel('relative CEP (rad)', size=s, color='navy')
     plt.tick_params(axis='both',labelsize=s)
     plt.tick_params(axis='y',labelcolor ='navy', color = 'navy')
-    target = 0   #CEP target value
-    plt.axhline(target, color = 'grey', alpha = 0.8, linewidth = 1)  #horizontal line = target value
-    for n in np.arange(1,11):  # Puts some horizontal lines every 2Pi starting from target value
-        plt.axhline(target + n*2*np.pi, color = 'grey', alpha = 0.2, linewidth = 1)
-        plt.axhline(target - n*2*np.pi, color = 'grey', alpha = 0.2, linewidth = 1)
-    #plt.ylim([-np.pi,np.pi])
-    #plt.yticks([-np.pi, -np.pi/2,0,np.pi/2, np.pi], ['-$\pi$','-$\pi$/2', '0', '$\pi$/2','$\pi$' ], size=s)
-        
+    if lines2pi == 1: # Puts some horizontal lines every 2Pi starting from target value
+        target = 0   #CEP target value
+        plt.axhline(target, color = 'grey', alpha = 0.8, linewidth = 1)  #horizontal line = target value
+        upper = max(round(max(phase)/(2*np.pi)),  abs(round(min(phase)/(2*np.pi))) )
+        for n in np.arange(1,upper):  
+            plt.axhline(target + n*2*np.pi, color = 'grey', alpha = 0.2, linewidth = 1)
+            plt.axhline(target - n*2*np.pi, color = 'grey', alpha = 0.2, linewidth = 1)
+    if Pi_window == 1:
+        plt.ylim([-np.pi,np.pi])
+        plt.yticks([-np.pi, -np.pi/2,0,np.pi/2, np.pi], ['-$\pi$','-$\pi$/2', '0', '$\pi$/2','$\pi$' ], size=s)
+    elif Pi_window == 0:
+        plt.ylim(min(phase)-0.1*abs(min(phase)), max(phase)+0.1*max(phase) )
+    
     
     #Histogram
     histo = plt.subplot(gs[1,1])
@@ -134,11 +143,13 @@ for k in np.arange(0,np.size(indices)-1) :
     plt.axis('off')
     plt.xlabel('occurrence', size=s)
     plt.tick_params(axis='both',labelsize=s)
-    #plt.ylim([-np.pi,np.pi])
+    if Pi_window == 1:
+        plt.ylim([-np.pi,np.pi])
+    elif Pi_window == 0:
+        plt.ylim(min(phase)-0.1*abs(min(phase)), max(phase)+0.1*max(phase) )
     d,f=np.histogram(phase,bins=round(ecart_type/20))
     plt.text(0.1*max(d), max(phase)-0.1*max(phase) ,'$\sigma_{rms}$ = ' + str(ecart_type) + ' mrad', size=s+1)
       
-    
     
     #FFT amplitude = f(time)
     ampli= plt.subplot(gs[0,0])
@@ -153,6 +164,7 @@ for k in np.arange(0,np.size(indices)-1) :
     plt.xlabel('time(sec)',size=s)
     plt.ylabel('Output voltage (V)', color='green', size=s)
     plt.tick_params(axis='y', labelcolor='green',color='green',labelsize=s)
+
 
 
     # title and save
