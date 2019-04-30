a2414d4ef128b216d13cd02346d5f1be094cf22c
Ouillz <marie.ouille@gmail.com>
2019-04-30 18:33:21 +0200

    Update APS800_PhaseAmplitudeVoltage.py

--- APS800/APS800_PhaseAmplitudeVoltage.py
+++ APS800/APS800_PhaseAmplitudeVoltage.py
@@ -9,6 +9,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.gridspec as gridspec
 import os
+from itertools import takewhile
 
 ## FIRST : EXTRACT DATA 
 #print ('\n','Where is the file located?  (e.g : Z:\Laser\CEP\20190311)')
@@ -16,8 +17,8 @@ import os
 #print ('\n', 'What is the name of the .dat file?  (e.g : 20190301_CEPdata) ')
 #filename = input('The name of the file is :  ')
 
-filepath = r'Z:\Laser\CEP\20190425'
-filename ='APS800_HCF-1bar_3-5fsdscan_noCMinf2f_nofeedback_oscillatorlocked'
+filepath = r'Z:\Laser\CEP\20190430'
+filename ='30ms'
 
 
 
@@ -95,83 +96,206 @@ def APS (filepath, filename, n, save=1, picenter=0, lines2pi=0):
         amp.append(float(p.split('\t')[3]))
         voltage.append(float(p.split('\t')[6]))
         squared_phase.append(phase[k]*phase[k])
-      
-    #Phase deviation rms in mrad
-    mean_phase = sum(phase)/np.size(phase)
-    mean_squared_phase = sum(squared_phase)/np.size(squared_phase)
-    ecart_type = int(round(np.sqrt(mean_squared_phase - mean_phase*mean_phase),3)*1000)
-    
-    #center values on 0 
-    for i in np.arange(0,np.size(phase)):
-        phase[i]=phase[i]-mean_phase
-       
-    #Figure parameters
-    fig=plt.figure(figsize=(12,7))
-    gs = gridspec.GridSpec(3, 2, wspace = 0,  width_ratios=[3, 1])
-    s=12
+
         
-    #CEP as a function of time
-    plt.subplot(gs[1,0])
-    plt.plot(time,phase, '.', markersize=1.2,  color='navy')
-    plt.ylabel('relative CEP (rad)', size=s, color='navy')
-    plt.tick_params(axis='both',labelsize=s)
-    plt.tick_params(axis='y',labelcolor ='navy', color = 'navy')
-    if lines2pi == 1: # Puts some horizontal lines every 2Pi starting from target value
-        target = 0   #CEP target value
-        plt.axhline(target, color = 'grey', alpha = 0.8, linewidth = 1)  #horizontal line = target value
-        upper = max(round(max(phase)/(2*np.pi)),  abs(round(min(phase)/(2*np.pi))) )
-        for n in np.arange(1,upper):  
-            plt.axhline(target + n*2*np.pi, color = 'grey', alpha = 0.2, linewidth = 1)
-            plt.axhline(target - n*2*np.pi, color = 'grey', alpha = 0.2, linewidth = 1)
-    if picenter == 1:
-        plt.ylim([-np.pi,np.pi])
-        plt.yticks([-np.pi, -np.pi/2,0,np.pi/2, np.pi], ['-$\pi$','-$\pi$/2', '0', '$\pi$/2','$\pi$' ], size=s)
-    elif picenter == 0:
-        plt.ylim(min(phase)-0.1*abs(min(phase)), max(phase)+0.1*max(phase) )
-    
-    #Histogram
-    plt.subplot(gs[1,1])
-    plt.hist(phase, bins=round(ecart_type/20), orientation='horizontal', color='navy')  #bins =.... number of columns
-    plt.yticks([])
-    plt.axis('off')
-    plt.xlabel('occurrence', size=s)
-    plt.tick_params(axis='both',labelsize=s)
-    if picenter== 1:
-        plt.ylim([-np.pi,np.pi])
-    elif picenter == 0:
-        plt.ylim(min(phase)-0.1*abs(min(phase)), max(phase)+0.1*max(phase) )
-    d,f=np.histogram(phase,bins=round(ecart_type/20))
-    plt.title('$\sigma_{rms}$ = ' + str(ecart_type) + ' mrad', size=s+1)
-    
-    #FFT amplitude = f(time)
-    plt.subplot(gs[0,0])
-    plt.plot(time, amp, '.',markersize=1.2, color = 'red')
-    plt.ylabel('FFT amplitude', color='red', size=s)
-    plt.tick_params(axis='y', labelcolor='red',color='red')
-    plt.tick_params(axis='both',labelsize=s)    
-    
-    # Output voltage = f(time)
-    plt.subplot(gs[2,0])
-    plt.plot(time, voltage, '.',markersize=1.2, color = 'green')
-    plt.xlabel('time(sec)',size=s)
-    plt.ylabel('Output voltage (V)', color='green', size=s)
-    plt.tick_params(axis='y', labelcolor='green',color='green')
-    plt.tick_params(axis='both',labelsize=s)
-    
-    # title and save
-    fig.suptitle('file : ' + str(files) + '\n' + 'Measurement n°' + str(n+1) + '/' +str(np.size(debuts)), size = s -4)
-    if save == 1:
-        plt.savefig( str(output_dir) + '\\' + filepath[-8::] + '_' + str(filename)+ '_measnum' +str(n) + '_FFTamplitude.png', dpi=900,  bbox_inches='tight')
-        print ('A .png file has been saved in the folder "analyzed_data" with 3 plots : FFT amplitude, CEP and Output voltage versus time','\n','\n','You will find the "analyzed_data" folder in your computer at the same lacoation as this .py file you are executing right now!' )
         
+             
+        
+        
+        
+    if partial == 0 :    
+        #Phase deviation rms in mrad
+        mean_phase = sum(phase)/np.size(phase)
+        mean_squared_phase = sum(squared_phase)/np.size(squared_phase)
+        ecart_type = int(round(np.sqrt(mean_squared_phase - mean_phase*mean_phase),3)*1000)
+        
+        #center values on 0 
+        for i in np.arange(0,np.size(phase)):
+            phase[i]=phase[i]-mean_phase
+           
+        #Figure parameters
+        fig=plt.figure(figsize=(12,7))
+        gs = gridspec.GridSpec(3, 2, wspace = 0,  width_ratios=[3, 1])
+        s=12
+            
+        #CEP as a function of time
+        plt.subplot(gs[1,0])
+        plt.plot(time,phase, '.', markersize=1.2,  color='navy')
+        plt.ylabel('relative CEP (rad)', size=s, color='navy')
+        plt.tick_params(axis='both',labelsize=s)
+        plt.tick_params(axis='y',labelcolor ='navy', color = 'navy')
+        if lines2pi == 1: # Puts some horizontal lines every 2Pi starting from target value
+            target = 0   #CEP target value
+            plt.axhline(target, color = 'grey', alpha = 0.8, linewidth = 1)  #horizontal line = target value
+            upper = max(round(max(phase)/(2*np.pi)),  abs(round(min(phase)/(2*np.pi))) )
+            for n in np.arange(1,upper):  
+                plt.axhline(target + n*2*np.pi, color = 'grey', alpha = 0.2, linewidth = 1)
+                plt.axhline(target - n*2*np.pi, color = 'grey', alpha = 0.2, linewidth = 1)
+        if picenter == 1:
+            plt.ylim([-np.pi,np.pi])
+            plt.yticks([-np.pi, -np.pi/2,0,np.pi/2, np.pi], ['-$\pi$','-$\pi$/2', '0', '$\pi$/2','$\pi$' ], size=s)
+        elif picenter == 0:
+            plt.ylim(min(phase)-0.1*abs(min(phase)), max(phase)+0.1*max(phase) )
+        
+        #Histogram
+        plt.subplot(gs[1,1])
+        plt.hist(phase, bins=round(ecart_type/20), orientation='horizontal', color='navy')  #bins =.... number of columns
+        plt.yticks([])
+        plt.axis('off')
+        plt.xlabel('occurrence', size=s)
+        plt.tick_params(axis='both',labelsize=s)
+        if picenter== 1:
+            plt.ylim([-np.pi,np.pi])
+        elif picenter == 0:
+            plt.ylim(min(phase)-0.1*abs(min(phase)), max(phase)+0.1*max(phase) )
+        d,f=np.histogram(phase,bins=round(ecart_type/20))
+        plt.title('$\sigma_{rms}$ = ' + str(ecart_type) + ' mrad', size=s+1)
+        
+        #FFT amplitude = f(time)
+        plt.subplot(gs[0,0])
+        plt.plot(time, amp, '.',markersize=1.2, color = 'red')
+        plt.ylabel('FFT amplitude', color='red', size=s)
+        plt.tick_params(axis='y', labelcolor='red',color='red')
+        plt.tick_params(axis='both',labelsize=s)    
+        
+        # Output voltage = f(time)
+        plt.subplot(gs[2,0])
+        plt.plot(time, voltage, '.',markersize=1.2, color = 'green')
+        plt.xlabel('time(sec)',size=s)
+        plt.ylabel('Output voltage (V)', color='green', size=s)
+        plt.tick_params(axis='y', labelcolor='green',color='green')
+        plt.tick_params(axis='both',labelsize=s)
+        
+        # title and save
+        fig.suptitle('file : ' + str(files) + '\n' + 'Measurement n°' + str(n+1) + '/' +str(np.size(debuts)), size = s -4)
+        if save == 1:
+            plt.savefig( str(output_dir) + '\\' + str(filename)+ '_measnum' +str(n) + '.png', dpi=900,  bbox_inches='tight')
+            print ('A .png file has been saved in the folder "analyzed_data" with 3 plots : FFT amplitude, CEP and Output voltage versus time','\n','\n','You will find the "analyzed_data" folder in your computer at the same lacoation as this .py file you are executing right now!' )
+            
+    
+  
+            
+            
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
      
         
+
+    #FOR PARTIAL PLOTS
+    elif partial == 1:
         
+        def index(l, p): return len([x for x in takewhile(lambda x: x[1] <= p, enumerate(l))])
+        a = index(time, start)
+        b = index(time, end)
+    
         
+            
+        #Phase deviation rms in mrad
+        mean_phase = sum(phase[a:b])/np.size(phase[a:b])
+        mean_squared_phase = sum(squared_phase[a:b])/np.size(squared_phase[a:b])
+        ecart_type = int(round(np.sqrt(mean_squared_phase - mean_phase*mean_phase),3)*1000)
         
+        #center values on 0 
+        for i in np.arange(0,np.size(phase)):
+            phase[i]=phase[i]-mean_phase
+           
+        #Figure parameters
+        fig=plt.figure(figsize=(12,7))
+        gs = gridspec.GridSpec(3, 2, wspace = 0,  width_ratios=[3, 1])
+        s=12
+            
+        #CEP as a function of time
+        plt.subplot(gs[1,0])
+        plt.plot(time[a:b],phase[a:b], '.', markersize=1.2,  color='navy')
+        plt.ylabel('relative CEP (rad)', size=s, color='navy')
+        plt.tick_params(axis='both',labelsize=s)
+        plt.tick_params(axis='y',labelcolor ='navy', color = 'navy')
+        if lines2pi == 1: # Puts some horizontal lines every 2Pi starting from target value
+            target = 0   #CEP target value
+            plt.axhline(target, color = 'grey', alpha = 0.8, linewidth = 1)  #horizontal line = target value
+            upper = max(round(max(phase[a:b])/(2*np.pi)),  abs(round(min(phase[a:b])/(2*np.pi))) )
+            for n in np.arange(1,upper):  
+                plt.axhline(target + n*2*np.pi, color = 'grey', alpha = 0.2, linewidth = 1)
+                plt.axhline(target - n*2*np.pi, color = 'grey', alpha = 0.2, linewidth = 1)
+        if picenter == 1:
+            plt.ylim([-np.pi,np.pi])
+            plt.yticks([-np.pi, -np.pi/2,0,np.pi/2, np.pi], ['-$\pi$','-$\pi$/2', '0', '$\pi$/2','$\pi$' ], size=s)
+        elif picenter == 0:
+            plt.ylim(min(phase[a:b])-0.1*abs(min(phase[a:b])), max(phase[a:b])+0.1*max(phase[a:b]) )
+        
+        #Histogram
+        plt.subplot(gs[1,1])
+        plt.hist(phase[a:b], bins=round(ecart_type/20), orientation='horizontal', color='navy')  #bins =.... number of columns
+        plt.yticks([])
+        plt.axis('off')
+        plt.xlabel('occurrence', size=s)
+        plt.tick_params(axis='both',labelsize=s)
+        if picenter== 1:
+            plt.ylim([-np.pi,np.pi])
+        elif picenter == 0:
+            plt.ylim(min(phase[a:b])-0.1*abs(min(phase[a:b])), max(phase[a:b])+0.1*max(phase[a:b]) )
+        d,f=np.histogram(phase,bins=round(ecart_type/20))
+        plt.title('$\sigma_{rms}$ = ' + str(ecart_type) + ' mrad', size=s+1)
+        
+        #FFT amplitude = f(time)
+        plt.subplot(gs[0,0])
+        plt.plot(time[a:b], amp[a:b], '.',markersize=1.2, color = 'red')
+        plt.ylabel('FFT amplitude', color='red', size=s)
+        plt.tick_params(axis='y', labelcolor='red',color='red')
+        plt.tick_params(axis='both',labelsize=s)    
+        
+        # Output voltage = f(time)
+        plt.subplot(gs[2,0])
+        plt.plot(time[a:b], voltage[a:b], '.',markersize=1.2, color = 'green')
+        plt.xlabel('time(sec)',size=s)
+        plt.ylabel('Output voltage (V)', color='green', size=s)
+        plt.tick_params(axis='y', labelcolor='green',color='green')
+        plt.tick_params(axis='both',labelsize=s)
+        
+        # title and save
+        fig.suptitle('file : ' + str(files) + '\n' + 'Measurement n°' + str(n+1) + '/' +str(np.size(debuts)), size = s -4)
+        if save == 1:
+            plt.savefig( str(output_dir) + '\\' + str(filename)+ '_measnum' +str(n) + '_interval_' + str(start) +'_'+str(end) + '.png', dpi=900,  bbox_inches='tight')
+            print ('A .png file has been saved in the folder "analyzed_data" with 3 plots : FFT amplitude, CEP and Output voltage versus time','\n','\n','You will find the "analyzed_data" folder in your computer at the same lacoation as this .py file you are executing right now!' )
+            
+    
+    
+    
+    
+    
+    
+    
+  
+    
+    
+    
+    
+    
+    
+
+#PARTIAL PLOT PARAMATERS 
+partial = 0 #1 if YES, you want to make a partial plot   
+#0 if NO you don't want to make a partial plot
+start = 0  #begining of the plot in seconds
+end = 10 #end of the plot in seconds 
 
 
 
-
-
-APS (filepath, filename, 0)
+APS (filepath, filename, 0)   #the last argument is the measurement number ; Be careful, it starts with 0 !!!
