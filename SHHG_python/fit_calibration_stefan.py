35ac47ca1aeff42622bcb3bf14b7f787d4c879c7
Ouillz <marie.ouille@gmail.com>
2020-02-24 16:13:02 +0100

    Update fit_calibration_stefan.py

--- SHHG_python/fit_calibration_stefan.py
+++ SHHG_python/fit_calibration_stefan.py
@@ -12,78 +12,34 @@ Created on Wed Apr 17 15:11:34 2019
 #import libraries
 import numpy as np
 import scipy as sc
+import sys
 from matplotlib import pyplot as plt
+if not 'C:\\Users\\ouille\\Desktop\\GitHub_users\\GitHub_MarieOuille\\python_functions' in sys.path : # put the path of the files : "smooth.py" and "peakdet.py" here 
+    sys.path.insert(0,'C:\\Users\\ouille\\Desktop\\GitHub_users\\GitHub_MarieOuille\\python_functions')
+from phys_constants import ct
+
 
 
 #define the function
 def fit_calibration_stefan(nn,xn,deltaE,l,inc_angle,pxsize,gratinglines):
+    
 
-    hbar = 1.0545718e-34  #Planck constant 
-    c = 299792458  # speed of light
-    e = 1.6021766208e-19  #J = 1eV
-    
-    # Set up figure to receive data sets and fits
     plt.figure()
-#    set(f_,'Units','Pixels','Position',[445 129 688 485]);
-#    # Line handles and text for the legend.
-#    legh_ = [];
-#    legt_ = {};
-#    # Limits of the x-axis.
-#    xlim_ = [Inf -Inf];
-#    # Axes for the plot.
-#    ax_ = axes;
-#    set(ax_,'Units','normalized','OuterPosition',[0 0 1 1]);
-#    set(ax_,'Box','on');
-#    axes(ax_);
-#    hold on;
-#    
-#    # --- Plot data that was originally in data set "xn vs. nn"
+
+    # --- Plot data that was originally in data set "xn vs. nn"
     plt.plot(nn,xn, 'o', label = 'xn (position) vs nn (offseted harmonic number)')
-#    nn = nn(:);
-#    xn = xn(:);
-#    h_ = line(nn,xn,'Parent',ax_,'Color',[0.333333 0 0.666667],...
-#        'LineStyle','none', 'LineWidth',1,...
-#        'Marker','.', 'MarkerSize',12);
-#    xlim_(1) = min(xlim_(1),min(nn));
-#    xlim_(2) = max(xlim_(2),max(nn));
-#    legh_(end+1) = h_;
-#    legt_{end+1} = 'xn vs. nn';
-#    
-#    # Nudge axis limits beyond data limits
-#    if all(isfinite(xlim_))
-#        xlim_ = xlim_ + [-1 1] * 0.01 * diff(xlim_);
-#        set(ax_,'XLim',xlim_)
-#    else
-#        set(ax_, 'XLim',[-0.41000000000000003, 41.409999999999997]);
-#    end
-#    
-    # --- Fit nn, xn to get C and B :
+
     
-    const1 = hbar /e *2*np.pi*c *gratinglines    # hbar/e * 2*pi*c /d
-    def expr (n, B, C):
-        return -l/pxsize / np.tan( np.arcsin( const1/((n+C)*deltaE) - np.sin(inc_angle/180*np.pi) )  ) - B/pxsize
-    [cf_,gof] = sc.optimize.curve_fit(expr, nn, xn)  #cf_ = [B, C]
+    const1 = ct.hbar /ct.e *2*np.pi*ct.c *gratinglines    # hbar/e * 2*pi*c /d
+    def expr (n, B,Coff):
+        return -l/pxsize / np.tan( np.arcsin( const1/((n+ Coff)*deltaE) - np.sin(inc_angle/180*np.pi) )  ) - B/pxsize
+    [cf_,gof] = sc.optimize.curve_fit(expr, nn, xn, bounds = ([0,5], [0.2,12] ) )  #cf_ = [B, C]
+
+
 
     plt.plot( np.arange(nn[-1], nn[0], 0.01) , expr( np.arange(nn[-1], nn[0], 0.01), cf_[0] ,cf_[1] ) , color= 'red' , label = 'fit')
     plt.legend()
-#    # Plot this fit
-#    h_ = plot(cf_,'fit',0.95);
-#    set(h_(1),'Color',[1 0 0],...
-#        'LineStyle','-', 'LineWidth',2,...
-#        'Marker','none', 'MarkerSize',6);
-#    # Turn off legend created by plot method.
-#    legend off;
-#    # Store line handle and fit name for legend.
-#    legh_(end+1) = h_(1);
-#    legt_{end+1} = 'fit 1';
-#    
-#    # --- Finished fitting and plotting data. Clean up.
-#    hold off;
-#    # Display legend
-#    leginfo_ = {'Orientation', 'vertical', 'Location', 'NorthEast'};
-#    h_ = legend(ax_,legh_,legt_,leginfo_{:});
-#    set(h_,'Interpreter','none');
-#    # Remove labels from x- and y-axes.
+
 
 
     return [cf_, gof]
